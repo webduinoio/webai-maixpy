56575baa04f67b0bf24869d36ee45727293a7a52
kevin <kevin@kingkit101.com>
2021-03-17 01:06:24 +0800

    [version] release beta version

--- components/micropython/port/include/mpconfigport.h
+++ components/micropython/port/include/mpconfigport.h
@@ -438,8 +438,8 @@ extern const struct _mp_obj_module_t mp_module_touchscreen;
 
 #define WebAI_K210_Major_Version "0"
 #define WebAI_K210_Minor_Version "1"
-#define WebAI_K210_Revision_Number "7"
-#define WebAI_K210_Build_Date "0312"
+#define WebAI_K210_Revision_Number "8"
+#define WebAI_K210_Build_Date "0317"
 #define WebAI_K210_Build_Number "01"
 
 #ifdef __linux__
