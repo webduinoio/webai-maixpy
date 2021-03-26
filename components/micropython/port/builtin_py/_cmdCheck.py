aafbcbc291e91e076d6c0d3196f3bd97adffbf75
kevin <kevin@kingkit101.com>
2021-03-26 18:21:38 +0800

    [flow] feat boot flow

---
+++ components/micropython/port/builtin_py/_cmdCheck.py
@@ -0,0 +1,7 @@
+if deployFlag:
+    print("deploy true,run main.py")
+else:
+    import webai_blockly
+    from machine import UART
+    webai_blockly.SYSTEM_LOG_UART = UART(UART.UART3, 115200, timeout=5000,read_buf_len=10240, callback=webai_blockly.MQTT_CALLBACK)
+    raise Exception("deploy false,run repl")
